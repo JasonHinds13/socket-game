61ede95973e992547a36c6ebec150f7a60d5e8a4
Javed Wright <jdwright870@gmail.com>
2020-05-04 21:46:44 -0500

    update db config based on env variable set

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,26 @@ from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from configparser import ConfigParser, ExtendedInterpolation
+from os import getenv
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
-app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app, cors_allowed_origins="*")
 
-# TODO: add config file with env options and local options
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+app_config = ConfigParser(interpolation=ExtendedInterpolation())
+app_config.read("app/config/config.ini")
+
+if app_config.get("PROFILE", "CURRENT_PROFILE") == 'dev':
+    app.config['SECRET_KEY'] = app_config.get("SERVER", "SECRET")
+    app.config['SQLALCHEMY_DATABASE_URI'] = app_config.get("DATABASE", "DB_URI")
+elif app_config.get("PROFILE", "CURRENT_PROFILE") == 'prod':
+    app.config['SECRET_KEY'] = getenv("SECRET")
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+    pass
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 db = SQLAlchemy(app)
 
