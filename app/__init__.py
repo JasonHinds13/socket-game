266fd04fa2cfa9117ed4f190cf70c426f8238c97
Javed Wright <jdwright870@gmail.com>
2020-05-05 12:14:12 -0500

    update config setup

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,16 @@ from os import getenv
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-app_config = ConfigParser(interpolation=ExtendedInterpolation())
-app_config.read("app/config/config.ini")
-
-if app_config.get("PROFILE", "CURRENT_PROFILE") == 'dev':
-    app.config['SECRET_KEY'] = app_config.get("SERVER", "SECRET")
-    app.config['SQLALCHEMY_DATABASE_URI'] = app_config.get("DATABASE", "DB_URI")
-elif app_config.get("PROFILE", "CURRENT_PROFILE") == 'prod':
+if getenv("USE_ENV_VAR"):
     app.config['SECRET_KEY'] = getenv("SECRET")
     app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
-    pass
+else:
+    app_config = ConfigParser(interpolation=ExtendedInterpolation())
+    app_config.read("app/config/config.ini")
+
+    app.config['SECRET_KEY'] = app_config.get("SERVER", "SECRET")
+    app.config['SQLALCHEMY_DATABASE_URI'] = app_config.get("DATABASE", "DB_URI")
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
 
