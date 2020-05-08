2d6dd59a1f55f83a6d9f245508e6ecf172dd71e5
Javed Wright <jdwright870@gmail.com>
2020-05-07 22:22:17 -0500

    create room and person entry when joining room

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from app import socketio, app
 from flask import render_template, request
 from flask_cors import cross_origin
 from flask_socketio import emit, join_room, leave_room
+from datetime import datetime
+
+from app import db
+from app.models.players import Persons
+from app.models.rooms import Rooms
 
 from app.cardHandler import getRandomQuestion, getAnswerCards
 
@@ -23,6 +28,30 @@ def on_join(data):
 	room = data['roomid']
 	join_room(room)
 	users[username] = request.sid
+
+	# TODO: Check if room exists, create room
+	room_obj = Rooms.query.filter_by(room_id=room).first()
+	if room_obj is None:
+		new_room = Rooms(room_id=room, is_active=True, is_open=True, number_of_users=1)
+	elif room_obj.is_active and room_obj.is_open:
+		room_obj.number_of_users += 1
+	else:
+		# emit join room error. room is closed
+		return
+
+	# TODO: Add user to room
+	person_obj = Persons.query.filter_by(player_name=username, current_room=room).first()
+
+	if person_obj is not None:
+		# emit('join_room_message', data, room=room)
+		return
+
+	person = Persons(player_name=username, current_room=room, game_points=0, last_active=datetime.now())
+	db.session.add(person)
+	if room_obj is None:
+		db.session.add(new_room)
+	db.session.commit()
+
 	data = {"message": username + ' has joined the room.'}
 	emit('join_room_message', data, room=room)
 
