f1b28590f9ecfee8135418c7c678dbb1141954ff
Javed Wright <jdwright870@gmail.com>
2020-06-03 23:55:06 -0500

    configure logging

--- app/__init__.py
+++ app/__init__.py
@@ -5,18 +5,40 @@ from flask_sqlalchemy import SQLAlchemy
 from configparser import ConfigParser, ExtendedInterpolation
 from os import getenv
 
+from logging.config import dictConfig
+
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 if getenv("USE_ENV_VAR"):
     app.config['SECRET_KEY'] = getenv("SECRET")
     app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+    log_level = getenv("ROOT_LOG_LEVEL")
+    default_format = getenv('DEFAULT_FORMAT')
 else:
     app_config = ConfigParser(interpolation=ExtendedInterpolation())
     app_config.read("app/config/config.ini")
 
     app.config['SECRET_KEY'] = app_config.get("SERVER", "SECRET")
     app.config['SQLALCHEMY_DATABASE_URI'] = app_config.get("DATABASE", "DB_URI")
+    log_level = app_config.get('LOGGER', 'ROOT_LOG_LEVEL')
+    default_format = app_config.get('LOGGER', 'DEFAULT_FORMAT')
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+            'format': default_format,
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': log_level,
+        'handlers': ['wsgi']
+    }
+})
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
