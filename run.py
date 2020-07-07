66b6c19f0254b8869d0deb3ebb7731970960be95
Javed Wright <jdwright870@gmail.com>
2020-07-07 15:17:08 -0500

    add interval config for room data removal

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import socketio, app
 from app.scheduler import database_cleanup
 
-database_cleanup.initiate_schedule(app.config['SCHEDULER_ROOM_DEACTIVATION'])
+database_cleanup.initiate_schedule(app.config['SCHEDULER_ROOM_DEACTIVATION'], app.config['SCHEDULER_ROOM_DELETION'])
 socketio.run(app, host='0.0.0.0')
