71cefc2ed849a753eb4a6779ea8fc0f80adc2039
Javed Wright <jdwright870@gmail.com>
2020-07-13 16:21:47 -0500

    Added game reset votes
    Added a column to indicate the number of players
    that have voted on the game reset

--- app/models/rooms.py
+++ app/models/rooms.py
@@ -11,4 +11,5 @@ class Rooms(db.Model):
     is_open = db.Column(db.Boolean)
     number_of_users = db.Column(db.Integer)
     game_reset_initiated = db.Column(db.Boolean, default=False)
+    game_reset_votes = db.Column(db.Integer, nullable=False, default=0)
     last_activity_check = db.Column(db.DateTime, server_default=func.now())
