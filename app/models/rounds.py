908b80e76ada8bf7ee6047cff6674a2872ac3120
Javed Wright <jdwright870@gmail.com>
2020-08-03 15:49:13 -0500

    Add foreign key constraint to rounds

--- app/models/rounds.py
+++ app/models/rounds.py
@@ -5,7 +5,7 @@ class Rounds(db.Model):
 
     __tablename__ = "rounds"
 
-    room_id = db.Column(db.String(255), primary_key=True)
+    room_id = db.Column(db.String(255), db.ForeignKey('rooms.room_id'), primary_key=True)
     round_number = db.Column(db.Integer, primary_key=True)
     question_card_holder = db.Column(db.String(255))
     number_of_answer_cards = db.Column(db.Integer)
