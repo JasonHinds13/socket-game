93590ba11712dbf7bd7733353c4a92a8159405ff
Javed Wright <jdwright870@gmail.com>
2020-08-03 14:52:25 -0500

    Add foreign key constraint

--- app/models/players.py
+++ app/models/players.py
@@ -6,7 +6,7 @@ class Persons(db.Model):
     __tablename__ = 'persons'
 
     player_name = db.Column(db.String(255), primary_key=True)
-    current_room = db.Column(db.String(255), primary_key=True)
+    current_room = db.Column(db.String(255), db.ForeignKey('rooms.room_id'), primary_key=True)
     game_points = db.Column(db.String(255), default=0)
     game_reset_vote = db.Column(db.Boolean, default=False)
     last_active = db.Column(db.DateTime)
