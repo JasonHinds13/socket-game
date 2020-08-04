5de1464e554e4cf71a9d921647213c4804a59d83
Javed Wright <jdwright870@gmail.com>
2020-08-04 13:43:27 -0500

    Add foreign key constraint to card_history_model
    left out player_name constraint until database and models can be
    properly normalized

--- app/models/cards_played.py
+++ app/models/cards_played.py
@@ -1,4 +1,5 @@
 from app import db
+from app.models.rounds import Rounds
 
 
 class CardHistory(db.Model):
@@ -11,3 +12,5 @@ class CardHistory(db.Model):
     room_id = db.Column(db.String(255))
     card_played = db.Column(db.Text)
     card_type = db.Column(db.String(255))
+
+    __table_args__ = (db.ForeignKeyConstraint([round_number, room_id], [Rounds.round_number, Rounds.room_id]), {})
